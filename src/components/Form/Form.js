013ba2a443c235deeca1a72d8e154e766d4a9456
JekIquin <jci.dev.0@gmail.com>
2021-08-21 16:56:29 -0700

    added error handling on form event and error border color

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,8 +6,21 @@ import PageButton from '../PageButton/PageButton';
 
 
 function Form() {
+    const handleSubmit = event => {
+        event.preventDefault();
+        const commentInput = event.target.comment;
+        if (commentInput.value.trim() === '') {
+            commentInput.classList.add('comments__form-input--error');
+            return
+        }
+        commentInput.classList.remove('comments__form-input--error');
+
+        console.log(event.target.comment.value);        //placeholder for submit handler prop
+        event.target.reset();
+    }
+
     return (
-        <form className="comments__form">
+        <form className="comments__form" onSubmit={handleSubmit}>
             <div className="comments__form-input-container">
                 <label htmlFor="comment" className="comments__form-label">Join the Conversation</label>
                 <textarea className="comments__form-input" name="comment" id="comment" placeholder="Please enter comment" maxLength="250"></textarea>
