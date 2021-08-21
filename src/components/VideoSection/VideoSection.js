a980d17f3aa252f6ad6ee82f8e0e8b089a7d87c4
JekIquin <jci.dev.0@gmail.com>
2021-08-21 15:37:28 -0700

    initial refactor

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,7 +1,6 @@
 import './VideoSection.scss';
 
 function VideoSection({ selectedVideo, selectedPoster }) {
-    console.log(selectedVideo)
     return (
         <section className="content__video-container">
             <video className="content__video" src={selectedVideo} poster={selectedPoster} controls></video>
