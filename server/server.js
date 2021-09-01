774f7955c69364c91c11da9ae4f473868a6d3d6f
JekIquin <jci.dev.0@gmail.com>
2021-09-01 15:13:57 -0700

    added public images to server middleware

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const app = express();
 const cors = require('cors');
 
 require('dotenv').config();
-
 const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public/images'));
 
 app.listen(PORT, () => {
     console.log(`Port open at ${PORT}.`);
