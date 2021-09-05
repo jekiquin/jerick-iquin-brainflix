815e25766115651abc2d393854659a085989e24e
JekIquin <jci.dev.0@gmail.com>
2021-09-04 23:13:25 -0700

    added delete route on server side

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -10,6 +10,10 @@ router.post('/:videoId/comments', (req, res) => {
         const {videoId} = req.params;
         const videoList = readData();
         const selectedVideoIndex = videoList.findIndex(video => video.id === videoId);
+        if (selectedVideoIndex === -1) {
+            return res.status(400).json({message: `Unable to find index from id ${videoId}`})
+        }
+
         const comment = {
             ...req.body,
             id: uniqid(),
@@ -23,7 +27,32 @@ router.post('/:videoId/comments', (req, res) => {
         console.log(err);
         res.status(500).send('Internal database error');
     }
- 
+})
+
+router.delete('/:videoId/comments/:commentId', (req, res) => {
+    try{
+        const {videoId, commentId} = req.params;
+        const videoList = readData();
+        const selectedVideoIndex = videoList.findIndex(video => video.id === videoId);
+        if (selectedVideoIndex === -1) {
+            return res.status(400).json({message: `Unable to find index from id ${videoId}`})
+        }
+
+        const selectedVideo = videoList[selectedVideoIndex]
+        const selectedCommentIndex = selectedVideo.comments.findIndex(comment => comment.id === commentId);
+        const selectComment = selectedVideo.comments[selectedCommentIndex];
+        if (selectedCommentIndex === -1) {
+            return res.status(400).json({message: `Unable to find index from id ${commentId}`})
+        }
+
+        videoList[selectedVideoIndex].comments.splice(selectedCommentIndex);
+        writeFile(videoList);
+        res.status(200).json(selectComment);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send('Internal database error');
+    }
 })
 
 module.exports = router;
